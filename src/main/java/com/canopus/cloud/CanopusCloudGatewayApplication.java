97057e2e0e79eedf70fa0f59538190e24471c97a
mharawi <mharawi@atypon.com>
2023-05-19 03:58:03 +0300

    Integrating with Eureka

--- src/main/java/com/canopus/cloud/CanopusCloudGatewayApplication.java
+++ src/main/java/com/canopus/cloud/CanopusCloudGatewayApplication.java
@@ -4,9 +4,11 @@ import com.canopus.cloud.route.RouterProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties({RouterProperties.class})
+@EnableDiscoveryClient
 public class CanopusCloudGatewayApplication {
 
     public static void main(String[] args) {
